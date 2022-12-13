1231a5bfc14bf8a5e15ba1732e0f71ac46a62350
Omar Talat <dr.omartalat@gmail.com>
2022-12-13 07:58:15 -0800

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Description : checks for lowercase character.
+ *
+ * @c: the integer value it receives
+ *
+ * Return: 1 if true. 0 if false.
+ **/
+
+int _islower(int c)
+{
+int l;
+for (l = 'a'; l <= 'z'; l++)
+{
+if (l == c)
+{
+return (1);
+}
+}
+return (0)
+}
