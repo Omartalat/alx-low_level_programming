64c2a284f786f7f93793742e85563f9aba6c1dda
dr.omartalat <dr.omartalat@gmail.com>
2023-07-06 08:50:34 -0700

    0x1A-hash_tables tasks till 5

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - function that adds an element to the hash table.
+ * @ht: pointer to hash table.
+ * key: the key, string.
+ * value: the value associated with the key, string.
+ *
+ * Returns: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (!ht || !key || !value)
+		return(0);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
+	{
+		ht->array[index]->value = strdup(value);
+		return (1);
+	}
+	else
+	{
+		new_node = malloc(sizeof(hash_node_t));
+		if (!new_node)
+			return(0);
+		new_node->key = strdup(key);
+		new_node->value = strdup(value);
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
+		return (1);
+	}
+}
