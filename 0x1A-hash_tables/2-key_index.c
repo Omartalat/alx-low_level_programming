bf1e757f587f4262978630e7101e163fe5b68d48
dr.omartalat <dr.omartalat@gmail.com>
2023-07-07 07:34:45 -0700

    0x1A-hash_tables task 2 betty fixed

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,16 +1,16 @@
 #include "hash_tables.h"
 
 /**
- * int key_index - function that gives you the index of a key.
+ * key_index - function that gives you the index of a key.
  * @key: The key, string
  *       The key is unique in the HashTable
  * @size: is the size of the array of the hash table, unsigned long
  *
- * Returns: the index at which the key/value pair should be stored in
+ * Return: the index at which the key/value pair should be stored in
  *          the array of the hash table
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key)%size);
+	return (hash_djb2(key) % size);
 }
