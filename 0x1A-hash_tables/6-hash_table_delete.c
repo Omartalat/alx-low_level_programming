d7d9a49465254173bd6cf31b0bbf10523ac76a1c
dr.omartalat <dr.omartalat@gmail.com>
2023-07-07 08:21:35 -0700

    0x1A-hash_tables task 6 pointer fixed 3

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -4,28 +4,30 @@
  * hash_table_delete - a function that deletes a hash table.
  * @ht: pointer to the hash table
  *
- * Made by Dr_Omar
  */
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *node, *tmp;
 	unsigned long int i;
 
-	for (i = 0; i < ht->size; i++)
+	if (ht)
 	{
-		if (ht->array[i] != NULL)
+		for (i = 0; i < ht->size; i++)
 		{
-			node = ht->array[i];
-			while (node != NULL)
+			if (ht->array[i] != NULL)
 			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
+				node = ht->array[i];
+				while (node != NULL)
+				{
+					tmp = node->next;
+					free(node->key);
+					free(node->value);
+					free(node);
+					node = tmp;
+				}
 			}
 		}
-	}
 	free(ht->array);
 	free(ht);
+	}
 }
