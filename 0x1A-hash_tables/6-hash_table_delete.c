cffd03a7b602cf4c86bac696cb5cac0b68b6e182
dr.omartalat <dr.omartalat@gmail.com>
2023-07-07 08:11:53 -0700

    0x1A-hash_tables task 6 pointer fixed 2

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,23 +8,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        if (ht->array[i] != NULL)
-        {
-            hash_node_t *node = ht->array[i];
-            while (node != NULL)
-            {
-                hash_node_t *tmp = node->next;
-                free(node->key);
-                free(node->value);
-                free(node);
-                node = tmp;
-            }
-        }
-    }
-    free(ht->array);
-    free(ht);
+	head = ht;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
